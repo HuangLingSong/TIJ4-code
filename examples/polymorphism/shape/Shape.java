350702fb5304f592974cd042b12b38fcd23969eb
huanglingsong <1543427148@qq.com>
2018-04-19 16:17:29 +0800

    .

---
+++ examples/polymorphism/shape/Shape.java
@@ -0,0 +1,13 @@
+//: polymorphism/shape/Shape.java
+package polymorphism.shape;
+
+
+import static net.mindview.util.Print.print;
+
+public class Shape {
+  public void draw() {}
+  public void erase() {}
+  public void log(){
+    print("log shape");
+  }
+} ///:~
---
+++ examples/polymorphism/shape/Square.java
@@ -0,0 +1,9 @@
+//: polymorphism/shape/Square.java
+package polymorphism.shape;
+import static net.mindview.util.Print.*;
+
+public class Square extends Shape {
+  @Override
+  public void draw() { print("Square.draw()"); }
+  public void erase() { print("Square.erase()"); }
+} ///:~
